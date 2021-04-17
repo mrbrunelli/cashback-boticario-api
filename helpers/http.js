435fb499dc593a7f5074f5a5c4e8a1fdf6aef7f4
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2021-04-17 14:53:46 -0300

    feat: add http response helper

---
+++ helpers/http.js
@@ -0,0 +1,32 @@
+/**
+ * @param {Response} res - Http Response
+ * @param {any} data
+ */
+const ok = (res, data) => {
+  return res.json(data);
+};
+
+/**
+ * @param {Response} res - Http Response
+ * @param {string} message
+ */
+const badRequest = (res, message) => {
+  return res.status(400).json(message);
+};
+
+/**
+ * @param {Response} res - Http Response
+ */
+const notFound = (res) => {
+  return res.status(400).json({
+    error: "Ooops! Endpoint Not Found.",
+    message: "Check the API Documentation.",
+    docs: "https://github.com/mrbrunelli/cash-back-boticario-api/API_DOCS.md",
+  });
+};
+
+module.exports = {
+  ok,
+  badRequest,
+  notFound,
+};
