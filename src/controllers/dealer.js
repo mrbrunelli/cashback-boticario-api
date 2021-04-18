21bc0462c600ecc2ece31a70c87037797f566ef5
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2021-04-17 22:26:13 -0300

    refactor: create cashback binding with dealer

--- src/controllers/dealer.js
+++ src/controllers/dealer.js
@@ -1,21 +1,32 @@
 const knex = require("../database/connection");
 const { ok, badRequest } = require("../helpers/http");
+const { isValidDealer } = require("../services/dealer");
 
 const dealerController = () => {
   return {
     index: async (req, res) => {
       const dealers = await knex("dealer")
-        .select("name", "cpf", "email")
+        .select("id", "name", "cpf", "email")
         .orderBy("name");
       return ok(res, dealers);
     },
 
     save: async (req, res) => {
+      const trx = await knex.transaction();
       try {
+        if (!isValidDealer(req.body)) {
+          throw Error("Invalid Dealer fields provided.");
+        }
         const { name, email, cpf, password } = req.body;
-        await knex("dealer").insert({ name, email, cpf, password });
+        const dealer = await trx("dealer")
+          .insert({ name, email, cpf, password })
+          .returning();
+        await trx("cashback")
+          .insert({ dealer_id: dealer[0] });
+        await trx.commit();
         return ok(res, "Dealer Registered Successfully.");
       } catch (e) {
+        await trx.rollback();
         return badRequest(res, e.message);
       }
     },
