65b308900c104329c39231103fcc08aea397bd97
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2021-04-17 21:50:51 -0300

    feat: add orderRouter in app.js

--- src/app.js
+++ src/app.js
@@ -2,11 +2,12 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const { verifyJWT } = require("./middlewares/auth");
 const notFoundRouter = require("./routes/not-found");
 const indexRouter = require("./routes/index");
-const dealerRouter = require("./routes/dealer");
 const loginRouter = require("./routes/login");
-const { verifyJWT } = require("./middlewares/auth");
+const dealerRouter = require("./routes/dealer");
+const orderRouter = require("./routes/order");
 
 const app = express();
 
@@ -17,8 +18,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/dealer", verifyJWT, dealerRouter);
 app.use("/login", loginRouter);
+app.use("/dealer", verifyJWT, dealerRouter);
+app.use("/order", verifyJWT, orderRouter);
 app.use("*", notFoundRouter);
 
 module.exports = app;
