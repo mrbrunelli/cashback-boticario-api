fc236a87c458188fd55f8e1891188b4f66f889fa
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2021-04-21 15:21:48 -0300

    feat: implement cors

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 const { verifyJWT } = require("./middlewares/auth");
 const notFoundRouter = require("./routes/not-found");
 const indexRouter = require("./routes/index");
@@ -11,6 +12,7 @@ const orderRouter = require("./routes/order");
 
 const app = express();
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
